8ad6c118cd21ea02d7a9a76d0d6025f25f6821ff
Labib361 <ahsanulmahbub1999@gmail.com>
2025-06-06 02:14:47 +0600

    asynchronous done

--- module_2/fs.js
+++ module_2/fs.js
@@ -3,5 +3,17 @@
 //2. asyncronous
 //file read ->single thread ->event loop ->thread pool->task complete hobe
 const fs=require("fs");
+console.log("line 1");
+const text="Learning file system";
+fs.writeFileSync("./hello.txt",text);
+console.log("Line 3");
 const data=fs.readFileSync("./hello.txt",{encoding:"utf-8"});
+console.log("Line 4");
 console.log(data);
+fs.readFile("./hello.txt",{encoding:"utf-8"},(err,data)=>{
+    if(err){
+        console.error("Error reading file",err);
+        return;
+    }
+    console.log("File contents:",data);
+})
