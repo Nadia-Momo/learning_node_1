d71da98e33d502999e1fa4c1af435d370679a3bf
Labib361 <nadiamahbuba.webdevsoln@gmail.com>
2025-06-09 03:50:46 +0600

    TODO list done

--- To_do_App/server.js
+++ To_do_App/server.js
@@ -1,55 +1,26 @@
 const http=require("http");
 const path=require("path");
 const fs=require("fs");
-const filePath=path.join(__dirname,'./db/todo.json')
+const filePath=path.join(__dirname,'./db/todo.json');
+const url=new URL(req.url,`http://${req.headers.host}`);
+const pathname=url.pathname
+console.log(url,"url");
 const server=http.createServer((req,res)=>{
     //get all todos
      console.log(req.url,req.method);
 // res.end("Welcome to todo app server");
-if(req.url==="/todos" && req.method==='GET'){
+if(pathname==="/todos" && req.method==='GET'){
     const data=fs.readFileSync(filePath,{encoding:"utf-8"})
     res.writeHead(201,{
         "content-type":"application/json",
         "email":"ph@gmail.com"
 
     })
-    /**[
-     *  {
-     * "title":"prema",
-     * "body":"Learning node",
-     * "createDate:"5/05/2025 3:25:04 AM"
-     * },{
-     * "title":"prema",
-     * "body":"Learning node",
-     *  "createDate:"6/04/2023 9:10:00 PM"
-     * }
-    ]
-     * */ 
-// res.writeHead(201,{
-//     "content-type":"test/plain",
-//     "email":"ph@gmail.com"
-// })
-// res.setHeader("content-type","text/plain");
-// res.setHeader("email","ph2@gmail.com");
-// res.statusCode=201;
-// const data=
-// [
-//      {
-//      "title":"prisma",
-//       "body":"Learning node",
-//       "createDate":"5/05/2025, 3:25:04 AM"
-//       },
-//       {
-//      "title":"typescript",
-//       "body":"Learning node",
-//       "createDate":"6/04/2023,9:10:00 PM"
-//      }
-//     ]
-//post all todos
+  
 res.end(data);
 }
 //post a todo
-else if(req.url==="/todos/create-todo" && req.method==='POST'){
+else if(pathname==="/todos/create-todo" && req.method==='POST'){
     let data="";
     req.on("data",(chunk)=>{
 data=data+chunk;
@@ -67,10 +38,50 @@ data=data+chunk;
         })
        const parsedAllTodos=JSON.parse(allTodos);
         parsedAllTodos.push({title,body, createdAt})
-        fs.writeFileSync(filePath,JSON.stringify(parsedAllTodos),{encoding:"utf-8"})
+        fs.writeFileSync(filePath,JSON.stringify(parsedAllTodos,null,2),{encoding:"utf-8"})
         res.end(JSON.stringify({title,body,createdAt},null,2));
     })
+}
+else if(pathname==="/todos/update-todo" && req.method==='PATCH'){
+    const title=url.searchParams.get("title");
+    let data="";
+    req.on("data",(chunk)=>{
+data=data+chunk;
+    });
   
+    req.on("end",()=>{
+
+        const {body}=JSON.parse(data);
+
+      
+      
+        const allTodos=fs.readFileSync(filePath,{
+            encoding:"utf-8"
+
+        })
+        const todoIndex=parsedAllTodos.findIndex((todo)=>todo.title===title)
+        parsedAllTodos[todoIndex].body=body
+       const parsedAllTodos=JSON.parse(allTodos);
+        parsedAllTodos.push({title,body, createdAt})
+        fs.writeFileSync(filePath,JSON.stringify(parsedAllTodos,null,2),{encoding:"utf-8"})
+        res.end(JSON.stringify({title,body,createdAt},null,2));
+    })
+}
+else if(pathname.startsWith("/todo") && req.method==='GET'){
+    const title=url.searchParams.get("title");
+    console.log(title);
+    console.log(req.url,"single todo");
+    const data=fs.readFileSync(filePath,{encoding:"utf-8"});
+    const parseData=JSON.parse(data)
+    const todo=parseData.find((todo)=>todo.title===title)
+    const striingifiedToDo=JSON.stringify(todo);
+    // const data=fs.readFileSync(filePath,{encoding:"utf-8"})
+    // res.writeHead(201,{
+    //     "content-type":"application/json",
+    //     "email":"ph@gmail.com"
+
+//     // })
+res.end(striingifiedToDo);
 
 }
 else{
